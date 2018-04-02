701dcf4df77ff4db2b805ff2d127b9c5f100ce65
tracer <tracer@tracer.org>
2018-04-02 23:42:08 +0800

    > run lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/mylibc/printf.c
+++ src/mylibc/printf.c
@@ -2,18 +2,15 @@
 #include <stdarg.h>
 
 static char mp[] = "0123456789ABCDEF";
-static int printf_int(unsigned int d, int base, int length, int type){
-	int ret = 0, d2 = d;
-	if (base == 10){
-		d2 = (int)d;
-		if (d2 < 0){
-			d2 = -d2;
-			_putc('-'), ret++; 
-		}
+static int printf_d(int d, int length, int type){
+	int ret = 0, base = 10; 
+	if (d2 < 0){
+		d2 = -d2;
+		_putc('-'), ret++; 
 	}
 	char *ptr; char dig[64];
 	ptr = &dig[63]; *ptr = '\0';
-	for ( ; d2 != 0; *--ptr = mp[d2 % base], d2 /= base, ret++);
+	for ( ; d != 0; *--ptr = mp[d % base], d /= base, ret++);
 	while (length > ret && type >= 0){
 		char c; 
 		switch (type){
@@ -26,9 +23,29 @@ static int printf_int(unsigned int d, int base, int length, int type){
 	}
 	/*if d = 0*/ if (*ptr == '\0') *--ptr = '0', ret++;
 	while (*ptr) _putc(*ptr++);
-	
 	return ret;
 }
+
+static int printf_x(unsigned int d, int length, int type){
+	int ret = 0, base = 16;
+	char *ptr; char dig[64];
+	ptr = &dig[63]; *ptr = '\0';
+	for ( ; d != 0; *--ptr = mp[d % base], d /= base, ret++);
+	while (length > ret && type >= 0){
+		char c; 
+		switch (type){
+			case 0: c = '0'; break;
+			case 1: c = ' '; break;
+			default: c = ' ';
+		}
+		*--ptr = c; 
+		ret++; 
+	}
+	/*if d = 0*/ if (*ptr == '\0') *--ptr = '0', ret++;
+	while (*ptr) _putc(*ptr++);
+	return ret;
+}
+
 int printf(const char *fmt, ...) {
     /*
     for (; *fmt; fmt++) {
@@ -38,7 +55,7 @@ int printf(const char *fmt, ...) {
     int ret = 0;
 	va_list ap; 
 	const char* iter;
-    unsigned int d; char c; char* s; 
+	int d; unsigned int u; char c; char* s; 
     va_start(ap, fmt);
     for (iter = fmt; *iter != '\0'; ++iter){
 		if (*iter != '%'){
@@ -69,12 +86,12 @@ int printf(const char *fmt, ...) {
 				
 				case 'd':
 					d = va_arg(ap, int);
-					ret += printf_int(d, 10, len, type);
+					ret += printf_d(d, len, type);
 					break;
 
 				case 'x':
-					d = va_arg(ap, unsigned int);
-					ret += printf_int(d, 16, len, type);
+					u = va_arg(ap, unsigned int);
+					ret += printf_x(u, len, type);
 					break; 	
 			  
 				case 'c':
