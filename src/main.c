cdb61824ecf94e74bf9a615d6b59175405e665aa
tracer <tracer@tracer.org>
2018-04-02 10:50:08 +0800

    > run lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/main.c
+++ src/main.c
@@ -53,7 +53,9 @@ static void ata_test(_Device *dev);
 
 int main() {
   if (_ioe_init() != 0) _halt(1);
-  printf("_heap = [%08x, %08x)\n", _heap.start, _heap.end);
+  printf("_heap = [%x, %x)\n", _heap.start, _heap.end);
+  return 0;
+  //printf("_heap = [%08x, %08x)\n", _heap.start, _heap.end);
   for (int n = 1; ; n++) {
     _Device *dev = _device(n);
     if (!dev) break;
