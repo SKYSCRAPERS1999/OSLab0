91d7378c4e3d363fa3686ba47ecb5743574a31bb
tracer <tracer@tracer.org>
2018-04-02 17:14:26 +0800

    > run lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/main.c
+++ src/main.c
@@ -128,9 +128,9 @@ static void ata_test(_Device *dev) {
     printf("\n");
   }
   char a[20]; char b[20] = "hello world"; strcpy(a, b); printf("%s\n",a);
-  char mp[20] = "0123456789";
+  char mp[20] = "0123456789";srand(3323);
   char x[2] = {mp[rand()%5]}; char y[2] = {mp[rand()%5]}; 
-  strcat(a,x); strcat(b,y); srand(332);
+  strcat(a,x); strcat(b,y); 
   printf("%s | %s\n", x, y);
   printf("%d | %d\n", strlen(x), strlen(y));
   printf("%s | %s\n", a, b);
