c823db485e57bab1e391acaef1f64ba5ff4e4098
tracer <tracer@tracer.org>
2018-04-02 14:53:16 +0800

    > run lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ int printf_int(unsigned int d, int base, int length, int type){
 	char *ptr; char dig[64];
 	ptr = &dig[63]; *ptr = '\0';
 	for ( ; d != 0; *--ptr = mp[d % base], d /= base, ret++);
-	while (length >= ret && type >= 0){
+	while (length > ret && type >= 0){
 		char c; 
 		switch (type){
 			case 0: c = '0'; break;
