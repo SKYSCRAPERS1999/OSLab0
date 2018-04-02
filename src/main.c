46dff22eb571970b4c03065b1a5460b1a49df3d7
tracer <tracer@tracer.org>
2018-04-02 21:11:28 +0800

    > rucln lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/main.c
+++ src/main.c
@@ -148,5 +148,6 @@ static void ata_test(_Device *dev) {
   //printf("\n%d\n", uptime());
   printf("%d X %d\n", screen_width(), screen_height());
   unsigned int aa[1000]; memset(aa, 0xdd, sizeof(aa));
+  for(int i = 0; i < 1000; i++) printf("%08x\n", aa[i]);
   draw_rect(aa, 1, 1, 30, 30);
 }
