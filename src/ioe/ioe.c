442c5d4876ef1c4697a1ade90d16d2a9d9fb1c0c
tracer <tracer@tracer.org>
2018-04-02 19:10:44 +0800

    > run lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/ioe/ioe.c
+++ src/ioe/ioe.c
@@ -4,7 +4,13 @@
 
 uint32_t uptime(){
 	_UptimeReg uptime;
-	_Device *dev = _device(_DEV_TIMER);
-	dev->read(_DEVREG_TIMER_UPTIME, &uptime, sizeof(uptime));
-	return uptime.lo;
+	for (int n = 1; ; n++){
+		_Device *dev = _device(n);
+		if (!dev) break;
+		else if (dev->id == _DEV_TIMER){
+			dev->read(_DEVREG_TIMER_UPTIME, &uptime, sizeof(uptime));
+			return uptime.lo;
+		}
+	}
+	return 0;
 }
